8f54a1de38b48a3cfae55849c98b81389e34847f
Nitish <sharma.nitish@live.com>
2019-05-21 01:23:44 -0400

    fix

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ app.post('/payment', (req, res) => {
   accounts.credit.balance = accounts.credit.balance - parseInt(req.body.amount, 10);
   accounts.credit.available = parseInt(accounts.credit.available, 10) + parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json', accountsJSON, 'utf8'));
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
   res.render('payment', { message: "Payment Successful", account: accounts.credit });
 })
 
