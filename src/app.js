0bd3bc352f48022d685c08c98ad3b18603ec6102
Nitish <sharma.nitish@live.com>
2019-05-21 01:17:26 -0400

    fix

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.get('/transfer', (req, res) => {
 
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance, 10) - parseInt(req.body.amount, 10);
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
   res.render('transfer', { message: 'Transfer Completed' });
@@ -69,7 +69,7 @@ app.post('/payment', (req, res) => {
   accounts.credit.balance = accounts.credit.balance - parseInt(req.body.amount, 10);
   accounts.credit.available = parseInt(accounts.credit.available, 10) + parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json', accountsJSON, 'urf8'));
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json', accountsJSON, 'utf8'));
   res.render('payment', { message: "Payment Successful", account: accounts.credit });
 })
 
