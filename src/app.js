3cfd73c4af7cdea058f72a8547db9fb5d6f982f9
Nitish <sharma.nitish@live.com>
2019-05-21 01:12:16 -0400

    fix

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.get('/transfer', (req, res) => {
 
 app.post('/transfer', (req, res) => {
   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-  accounts[req.body.to].balance = parseInt(account[req.body.to].balance) - parseInt(req.body.amount, 10);
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) - parseInt(req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts);
   fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
   res.render('transfer', { message: 'Transfer Completed' });
